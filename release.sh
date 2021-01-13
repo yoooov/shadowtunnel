409ac3fef8330ccbb5c8a67b50e327eb1f74cb98
yoooov <yoooov@protonmail.com>
2021-01-13 19:34:22 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@
 VERSION=$(cat ./VERSION)
 VER="${VERSION}_$(date '+%Y%m%d%H%M%S')"
 RELEASE="release-${VERSION}"
-X="-X github.com/snail007/shadowtunnel/core.VERSION=$VER"
-TRIMPATH1="/Users/snail/go/src/github.com/snail007"
-TRIMPATH=$(dirname ~/go/src/github.com/snail007)/snail007
+X="-X github.com/yoooov/shadowtunnel/core.VERSION=$VER"
+TRIMPATH1="${GOPATH}/src/github.com/yoooov"
+TRIMPATH=$(dirname ~/go-workspace/src/github.com/yoooov)/yoooov
 if [ -d "$TRIMPATH1" ];then
 	TRIMPATH=$TRIMPATH1
 fi
